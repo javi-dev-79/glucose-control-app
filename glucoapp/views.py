73fcccf815621c19166366fcfec64d91c4449052
Javi <javi.dev.79@gmail.com>
2024-06-29 21:20:48 +0100

    Add tailwind configuration

--- glucoapp/views.py
+++ glucoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("Hola, mundo, bienvenidos a GlucoControlApp.")
+    contexto = {
+        'mensaje': 'Hola Mundo!'
+    }
+    return render(request, 'index.html', contexto)
